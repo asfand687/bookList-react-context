183ad41529e582fe9845752dc1e24a06fb9ecdd3
asfand687 <asfandyar687@gmail.com>
2021-09-16 16:54:55 +0500

    added the local storage data persistance

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,26 +1,17 @@
-import { createContext, useReducer } from 'react'
+import { createContext, useEffect, useReducer } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import BookReducer from './BookReducer'
 
 export const BookContext = createContext()
-const initialState = [
-	{ id: uuidv4(), title: 'The Kite Runner', author: 'Khaled Hosseini' },
-	{
-		id: uuidv4(),
-		title: 'A Thousand Splendid Suns',
-		author: 'Khaled Hosseini',
-	},
-	{ id: uuidv4(), title: 'The Fifth Mountain', author: 'Paulo Coelho' },
-	{
-		id: uuidv4(),
-		title: 'The Saint of Incipient Insanities',
-		author: 'Elif Shafak',
-	},
-]
 
 const BookContextProvider = ({ children }) => {
-	const [state, dispatch] = useReducer(BookReducer, initialState)
-
+	const [state, dispatch] = useReducer(BookReducer, [], () => {
+		const localData = localStorage.getItem('books')
+		return localData ? JSON.parse(localData) : []
+	})
+	useEffect(() => {
+		localStorage.setItem('books', JSON.stringify(state))
+	}, [state])
 	return (
 		<BookContext.Provider value={{ books: state, dispatch }}>
 			{children}
